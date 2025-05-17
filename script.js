9c3284b18013f963c1acab269b3841aa6527e1cf
hadleyliao <hadleyliao@gmail.com>
2025-05-17 21:41:38 +0800

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 當使用者點擊「☹ Error ☹」按鈕時，重新載入當前頁面
+function reloadPage() {
+    window.location.reload(); // 重新載入網頁
+}
+
+// 當使用者點擊「► Restart ◄」按鈕時，導向 Casio 官方網站（台灣）
+function gotoCasio() {
+    window.location.href = 'https://www.casio.com/tw/'; // 導向指定網址
+}
